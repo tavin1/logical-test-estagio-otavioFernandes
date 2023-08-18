38f80132904ccfcce3c0b1ab22818d80dbcf6985
tavin1 <otaviospa@gmail.com>
2023-08-18 20:48:55 -0300

    Correção do codigo para o teste

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,49 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.css'; // Certifique-se de ter um arquivo de estilos CSS correspondente
 
 function App() {
-  const [total, setTotal] = useState('');
-  const [divisor, setDivisor] = useState('');
-  const [renderedComponents, setRenderedComponents] = useState([]);
+  const [total, setTotal] = useState(1);
+  const [divisor, setDivisor] = useState(1);
+  const [renderizations, setRenderizations] = useState([]);
 
-  const handleRender = () => {
-    if (total && divisor) {
-      const parsedTotal = parseInt(total);
-      const parsedDivisor = parseInt(divisor);
-      if (parsedTotal && parsedDivisor) {
-        const renderArray = Array.from({ length: parsedDivisor }, (_, index) => (
-          <div key={index} className="rendered-component">
-            Renderização {index + 1} - Valor: {parsedTotal / parsedDivisor}
-          </div>
-        ));
-        setRenderedComponents(renderArray);
-      }
+  const handleRenderClick = () => {
+    if (total <= 0 || divisor <= 0) {
+      return;
     }
+
+    const renderElements = [];
+    for (let i = 0; i < divisor; i++) {
+      const result = (i + 1) * (total / divisor);
+      renderElements.push(
+        <p key={i}>Renderização {i + 1}: {result}</p>
+      );
+    }
+    setRenderizations(renderElements);
   };
 
   return (
-    <div className="App">
-      <h1>Renderização Condicional</h1>
-      <div>
-        <label>Total: </label>
-        <input
-          type="number"
-          value={total}
-          onChange={(e) => setTotal(e.target.value)}
-        />
+    <div className="container">
+      <h1>Divisor Renderizações em React</h1>
+      <label htmlFor="total">Valor Total:</label>
+      <input
+        type="number"
+        id="total"
+        value={total}
+        onChange={(e) => setTotal(parseInt(e.target.value))}
+        required
+      />
+      <label htmlFor="divisor">Divisor:</label>
+      <input
+        type="number"
+        id="divisor"
+        value={divisor}
+        onChange={(e) => setDivisor(parseInt(e.target.value))}
+        required
+      />
+      <button onClick={handleRenderClick}>Renderizar</button>
+      <div id="renderOutput">
+        {renderizations.map(renderElement => renderElement)}
       </div>
-      <div>
-        <label>Divisor: </label>
-        <input
-          type="number"
-          value={divisor}
-          onChange={(e) => setDivisor(e.target.value)}
-        />
-      </div>
-      <button onClick={handleRender}>Renderizar</button>
-      <div className="rendered-container">{renderedComponents}</div>
     </div>
   );
 }
